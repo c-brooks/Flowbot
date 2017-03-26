a09a85e42e2b1b926d410ccfbf1215b3a5a4a5fe
Corey <c.brooks96@live.ca>
2017-03-26 12:03:02 -0700

    Change from 2-space to 4-space indentation

--- ml/markov.go
+++ ml/markov.go
@@ -1,5 +1,5 @@
 // Markov Chain
-// This is a probablistic model of language that treats groups of letters
+// This is a probabilistic model of language that treats groups of letters
 // as nodes (3-letter groups for 3rd-order chain) and transitions
 // between groups as edges
 
@@ -16,48 +16,50 @@ import "fmt"
 type TransitionTable [27][27]float64
 
 
+/**
 
+ */
 func Train(song string, order int) {
-  var tt TransitionTable
-  AlphaLookup := map[byte]int{
-    'a': 0,
-    'b': 1,
-    'c': 2,
-    'd': 3,
-    'e': 4,
-    'f': 5,
-    'g': 6,
-    'h': 7,
-    'i': 8,
-    'j': 9,
-    'k': 10,
-    'l': 11,
-    'm': 12,
-    'n': 13,
-    'o': 14,
-    'p': 15,
-    'q': 16,
-    'r': 17,
-    's': 18,
-    't': 19,
-    'u': 20,
-    'v': 21,
-    'w': 22,
-    'x': 23,
-    'y': 24,
-    'z': 25,
-    ' ': 26,
-  }
+	var tt TransitionTable
+	AlphaLookup := map[byte]int{
+		'a': 0,
+		'b': 1,
+		'c': 2,
+		'd': 3,
+		'e': 4,
+		'f': 5,
+		'g': 6,
+		'h': 7,
+		'i': 8,
+		'j': 9,
+		'k': 10,
+		'l': 11,
+		'm': 12,
+		'n': 13,
+		'o': 14,
+		'p': 15,
+		'q': 16,
+		'r': 17,
+		's': 18,
+		't': 19,
+		'u': 20,
+		'v': 21,
+		'w': 22,
+		'x': 23,
+		'y': 24,
+		'z': 25,
+		' ': 26,
+  	}
 
-  for i := range song {
-    if i < len(song)-1 {
-      tt[AlphaLookup[song[i]]][AlphaLookup[song[i+1]]]++
-    }
-  }
+  	for i := range song {
+		if i < len(song)-1 {
+			tt[AlphaLookup[song[i]]][AlphaLookup[song[i+1]]]++
+    		}
+  	}
 
   // Print out tt
-  for i := range tt {
-      fmt.Println(tt[i] )
-  }
+  	for i := range tt {
+      		fmt.Println(tt[i] )
+	}
 
 }
--- scraper/scraper.go
+++ scraper/scraper.go
@@ -2,21 +2,21 @@ package scraper
 
 
 import (
-  "bytes"
-  "fmt"
-  "log"
-  "strings"
-  "github.com/PuerkitoBio/goquery"
+	"bytes"
+	"fmt"
+	"log"
+	"strings"
+	"github.com/PuerkitoBio/goquery"
 )
 
 func Scrape(artistName string) []string {
-  var songBuf bytes.Buffer
-  var retArr []string
-  for _, track := range scrapeTrackList("http://www.azlyrics.com/" + string(artistName[0]) + "/" + artistName + ".html") {
-    if track != "" {
-      geniusUrl := "https://genius.com/" + artistName + "-" + dasherize(track) + "-lyrics"
-      songBuf.WriteString(scrapeLyrics(geniusUrl))
-      retArr = append(retArr, songBuf.String())
+	var songBuf bytes.Buffer
+	var retArr []string
+	for _, track := range scrapeTrackList("http://www.azlyrics.com/" + string(artistName[0]) + "/" + artistName + ".html") {
+    	if track != "" {
+      		geniusUrl := "https://genius.com/" + artistName + "-" + dasherize(track) + "-lyrics"
+      		songBuf.WriteString(scrapeLyrics(geniusUrl))
+      		retArr = append(retArr, songBuf.String())
     }
   }
   return retArr
@@ -26,53 +26,52 @@ func Scrape(artistName string) []string {
 // Scrape Migos songs from http://www.azlyrics.com/m/migos.html
 // Return a list of tracks
 func scrapeTrackList(websiteUrl string) []string {
-  fmt.Println("GET [", websiteUrl, "]\n")
-  doc, err := goquery.NewDocument(websiteUrl)
-  if err != nil {
-    panic(err.Error())
-  }
+	fmt.Println("GET [", websiteUrl, "]\n")
+  	doc, err := goquery.NewDocument(websiteUrl)
+  	if err != nil {
+		panic(err.Error())
+	}
 
-  var trackList []string
-  doc.Find("#listAlbum > a").Each(func (i int, s *goquery.Selection) {
-    trackList = append(trackList, s.Text())
-  })
-  if len(trackList) == 0 {
-    log.Fatal("No tracks found!") // exit
-  }
-  return trackList
+	var trackList []string
+	doc.Find("#listAlbum > a").Each(func (i int, s *goquery.Selection) {
+		trackList = append(trackList, s.Text())
+	})
+	if len(trackList) == 0 {
+		log.Fatal("No tracks found!") // exit
+	}
+	return trackList
 }
 
 
 // Scrape lyrics from Genius
 // Print to standard output
 func scrapeLyrics(websiteUrl string) string {
-  fmt.Println("\t GET [", websiteUrl, "]\n")
-  doc, err := goquery.NewDocument(websiteUrl)
-  if err != nil {
-    panic(err.Error())
-  }
-
-  return formatLyrics(doc.Find(".lyrics").Text())
+	fmt.Println("\t GET [", websiteUrl, "]\n")
+	doc, err := goquery.NewDocument(websiteUrl)
+	if err != nil {
+		panic(err.Error())
+	}
+	return formatLyrics(doc.Find(".lyrics").Text())
 }
 
 // Change the track name into a url-friendly form
 // This includes removing some punctuation for Genius' standard urls
 func dasherize(track string) string {
-  r := strings.NewReplacer(" ", "-", "(", "", ")", "", "'", "", ".", "", "&", "and")
-  return r.Replace(track)
+	r := strings.NewReplacer(" ", "-", "(", "", ")", "", "'", "", ".", "", "&", "and")
+	return r.Replace(track)
 }
 
 // Formats lyrics into a cleaned-up string
 // Returns string
 func formatLyrics(lyrics string) string {
-  var retLyrics bytes.Buffer
+	var retLyrics bytes.Buffer
 
-  for _, line := range strings.Split(lyrics, "\n") {
+	for _, line := range strings.Split(lyrics, "\n") {
     // Test for unwanted lines
-    line = strings.Trim(line, " ")
-    if len(line) > 0 && string(line[0]) != "[" {
-      retLyrics.WriteString(line + " ")
-    }
-  }
-  return retLyrics.String()
+		line = strings.Trim(line, " ")
+		if len(line) > 0 && string(line[0]) != "[" {
+			retLyrics.WriteString(line + " ")
+		}
+	}
+	return retLyrics.String()
 }
