525c7db3c021c5a79c44a218b1cc9a08db030eda
Corey <c.brooks96@live.ca>
2017-03-26 12:03:02 -0700

    Change from 2-space to 4-space indentation

--- main.go
+++ main.go
@@ -1,27 +1,26 @@
 // main.go is the entry point for the program.
-// It sets up all necessary connections for the application.
 package main
 
 import (
-  "os"
-  "github.com/c-Brooks/bADLIB/scraper"
-  "fmt"
-  "github.com/c-Brooks/bADLIB/ml"
+	"os"
+	"github.com/c-Brooks/bADLIB/scraper"
+	"fmt"
+	"github.com/c-Brooks/bADLIB/ml"
 )
 
 func main() {
-  // Get artist name from command-line args
-  var artistName string
-  if len(os.Args) > 1 {
-    artistName = os.Args[1]
-  } else {
-    // Falback to a classic
-    artistName = "migos"
-  }
+	// Get artist name from command-line args
+	var artistName string
+	if len(os.Args) > 1 {
+		artistName = os.Args[1]
+	} else {
+		// Falback to a classic
+		artistName = "migos"
+	}
 
-  fmt.Println(artistName)
-  songArr := scraper.Scrape(artistName)
-    for song := range songArr {
-      ml.Train(songArr[song], 1)
-    }
+	fmt.Println(artistName)
+	songArr := scraper.Scrape(artistName)
+	for song := range songArr {
+		ml.Train(songArr[song], 1)
+	}
 }
